feae18c816b19f9fa75006e35a5710bb93d986b1
sonrisarco-co <sonrisarco@gmail.com>
2026-04-02 16:42:28 -0300

    debug carga postgres

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
+set -e
 
+echo "=== INSTALANDO ==="
 pip install -r requirements.txt
+
+echo "=== MIGRANDO ==="
 python manage.py migrate
+
+echo "=== CARGANDO DATOS ==="
 python manage.py loaddata datos.json
+
+echo "=== STATIC ==="
 python manage.py collectstatic --noinput
+
+echo "=== FIN BUILD ==="
