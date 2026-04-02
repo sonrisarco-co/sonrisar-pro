edf8aa06a0a928bfa1cb926c64c3e43a5af733fc
sonrisarco-co <sonrisarco@gmail.com>
2026-04-02 00:02:15 -0300

    configura postgres para render

--- backend/settings.py
+++ backend/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+import dj_database_url
 
 # --- BASE DIR ---
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,10 +66,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # --- BASE DE DATOS ---
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 # --- IDIOMA / HORARIO ---
