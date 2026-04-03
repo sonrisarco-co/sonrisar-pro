64edff0ab875b7e1e85394099f141f6ccea49254
sonrisarco-co <sonrisarco@gmail.com>
2026-04-02 22:10:41 -0300

    actualizar

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from core import views as core_views
 
-print("🚨 BACKEND URLS CARGADAS 🚨")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
