aa58af41d98647f12f058bb5c90f67a18dd25561
sonrisarco-co <sonrisarco@gmail.com>
2026-04-10 16:30:17 -0300

    quito bloque superuser de settings

--- backend/settings.py
+++ backend/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import os
 
 # ==========================================
 # BASE DIR
@@ -177,8 +176,3 @@ CSRF_TRUSTED_ORIGINS = [
 
 
 
-if os.environ.get("RENDER"):
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@email.com", "admin123")
