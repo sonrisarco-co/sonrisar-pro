f34511fa3f2c2a56614632de01e6b311674dc830
sonrisarco-co <sonrisarco@gmail.com>
2026-04-13 01:28:22 -0300

    Quitar restore peligroso del build

--- build.sh
+++ build.sh
@@ -5,13 +5,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
-if [ "$FORCE_CORE_RESTORE" = "1" ]; then
-  echo "FORCE_CORE_RESTORE=1 -> cargando backup_core_final.json"
-python manage.py loaddata backup_core_final_v2.json
-else
-  echo "Restore omitido"
-fi
-
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
   python manage.py createsuperuser --noinput || true
 fi
