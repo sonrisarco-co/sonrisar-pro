8dbdecf9d545d756309c5c74c6ff8b4cf064f039
sonrisarco-co <sonrisarco@gmail.com>
2026-04-13 23:38:50 -0300

    Fix migrate fake-initial

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py migrate --noinput
+python manage.py migrate --fake-initial --noinput
 
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
   python manage.py createsuperuser --noinput || true
