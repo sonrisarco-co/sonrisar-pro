d0b26da57a67dc1dbfb864eb3c982e576b8bd215
sonrisarco-co <sonrisarco@gmail.com>
2026-04-14 01:07:44 -0300

    Restaurar datos con backup limpio

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --fake-initial --noinput
-python manage.py loaddata backup_total.json --exclude contenttypes --exclude auth.permission --exclude admin.logentry --exclude sessions || true
-
+python manage.py loaddata backup_total.json
 
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
   python manage.py createsuperuser --noinput || true
