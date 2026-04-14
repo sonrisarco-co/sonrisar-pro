e8fe95cf74603cf1c3a79061f9986c4227e8cefd
sonrisarco-co <sonrisarco@gmail.com>
2026-04-14 01:25:21 -0300

    Excluir RayosX al restaurar backup

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --fake-initial --noinput
-python manage.py loaddata backup_total.json
+python manage.py loaddata backup_total.json --exclude core.RayosX
 
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
   python manage.py createsuperuser --noinput || true
