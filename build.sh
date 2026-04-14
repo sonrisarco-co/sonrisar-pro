5e416548b035352574c0745faaffae749736264d
sonrisarco-co <sonrisarco@gmail.com>
2026-04-14 00:25:15 -0300

    Permitir continuar si hay registros inválidos

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --fake-initial --noinput
-python manage.py loaddata backup_total.json --exclude contenttypes --exclude auth.permission --exclude admin.logentry --exclude sessions
+python manage.py loaddata backup_total.json --exclude contenttypes --exclude auth.permission --exclude admin.logentry --exclude sessions || true
+
 
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
   python manage.py createsuperuser --noinput || true
