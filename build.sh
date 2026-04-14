f5f49ba5357d7deca1cdc55d78464c52275c415b
sonrisarco-co <sonrisarco@gmail.com>
2026-04-14 00:12:35 -0300

    Restaurar datos desde backup

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --fake-initial --noinput
+python manage.py loaddata backup_total.json
 
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
   python manage.py createsuperuser --noinput || true
