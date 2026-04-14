595979e0955eb8d5178b1fb026d611d9ef11f14e
sonrisarco-co <sonrisarco@gmail.com>
2026-04-13 23:59:26 -0300

    Fix duplicate fecha_nacimiento migration

--- core/migrations/0022_fix_patient_fecha_nacimiento.py
+++ core/migrations/0022_fix_patient_fecha_nacimiento.py
@@ -1,18 +1,10 @@
-from django.db import migrations, models
+from django.db import migrations
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('core', '0017_patient_fecha_nacimiento'),
-]
-
-  
-
-    operations = [
-        migrations.AddField(
-            model_name="patient",
-            name="fecha_nacimiento",
-            field=models.DateField(null=True, blank=True),
-        ),
     ]
+
+    operations = []
