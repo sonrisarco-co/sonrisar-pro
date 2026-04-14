e8a341f028f4d862d77e42281fe16187475632f4
sonrisarco-co <sonrisarco@gmail.com>
2026-04-14 00:17:56 -0300

    Excluir tablas internas al restaurar backup

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --fake-initial --noinput
-python manage.py loaddata backup_total.json
+python manage.py loaddata backup_total.json --exclude contenttypes --exclude auth.permission --exclude admin.logentry --exclude sessions
 
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ]; then
   python manage.py createsuperuser --noinput || true
