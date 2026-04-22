eab7f90c00793f9146993e559c189e2633a5af09
sonrisarco-co <sonrisarco@gmail.com>
2026-04-21 23:45:40 -0300

    Servir archivos media para rayos x

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from core import views as core_views
 
 
@@ -11,7 +13,6 @@ urlpatterns = [
     path('api/', include('core.api.urls')),
 
     path("service-worker.js", core_views.service_worker, name="service-worker"),
-
-    
-
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
